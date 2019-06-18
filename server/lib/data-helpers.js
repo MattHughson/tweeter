537cfad98b567942e5c6e899ca492ed2e42202a5
MattHughson! <matt@koodomasson.com>
2019-06-18 15:49:43 +0000

    updated problems with git merge

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -2,26 +2,43 @@
 
 // Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
-
+function callback(str){
+  console.log("HERE")
+}
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
+  // console.log("test datahelpers", db)
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweet")
+      .insertOne(newTweet)
+      .then(() => callback(null, newTweet))
+      .catch(err => callback(err));
     },
 
     // Get all tweets in `db`, sorted by newest first
-    getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+
+    getTweets: function(callback){
+      const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+      db.collection("tweet")
+      .find()
+      .sort({ created_at: -1 })
+      .toArray()
+      .then(tweets => callback(null, tweets))
+      .catch(err => callback(err));
+   
+
     }
+    
+    
+    
+  
+   
+    
+
 
   };
+  
+
 }
